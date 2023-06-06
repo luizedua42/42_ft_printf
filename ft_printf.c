76c82cf433e4e915623f44c94e4908cfffdd0046
luizedua <zeexem@hotmail.com>
2023-06-06 20:54:08 -0300

    formatting main file

--- ft_printf.c
+++ ft_printf.c
@@ -6,40 +6,34 @@
 /*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:53:49 by luizedua          #+#    #+#             */
-/*   Updated: 2023/06/06 17:14:49 by luizedua         ###   ########.fr       */
+/*   Updated: 2023/06/06 20:52:04 by luizedua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "libftprintf.h"
-# include <stdlib.h>
-# include <stdarg.h>
-# include <unistd.h>
-
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int	ft_printf(const char *input, ...)
 {
 	int		i;
 	va_list	arg_lst;
-	
+
 	i = 0;
 	va_start(arg_lst, input);
 	while (input[i] != '\0')
 	{
-		if(input[i] == '%')
+		if (input[i] == '%')
 		{
 			i++;
 			if (input[i] == 'c')
 				ft_putchar(va_arg(arg_lst, int));
-			//if (input[i] == 's')
+			// if (input[i] == 's')
 			//	ft_putstr(va_arg(arg_lst, char *));
-			//if (input[i] == 'd')
-				// ft_putnbr(va_arg(arg_lst, int));
-			//if (input[i] == 'i')
+			// if (input[i] == 'd')
+			// ft_putnbr(va_arg(arg_lst, int));
+			// if (input[i] == 'i')
 			//	ft_putnbr(va_arg(arg_lst, int));
 		}
 		else
@@ -50,19 +44,19 @@ int	ft_printf(const char *input, ...)
 	return (0);
 }
 
-int main ()
+int	main(void)
 {
 	ft_printf("teste: %c", 'a');
 }
 
 //			if (input[i] == 'p')//0123456789abcdef
-				//converter em base 16 com 0x...(funcao dos x)
-			// if (input[i] == 'u')
-			//	ft_putunsint(va_arg(arg_lst, int));
-			// if (input[i] == 'x')//0123456789abcdef
-				//base 16 abcdef
-			// if (input[i] == 'X')//0123456789ABCDEF
-			//	base 16 ABCDEF
-			// if (input[i] == '%')
-			//	ft_putchar('%');
-			//somar o valor dos returns
+// converter em base 16 com 0x...(funcao dos x)
+// if (input[i] == 'u')
+//	ft_putunsint(va_arg(arg_lst, int));
+// if (input[i] == 'x')//0123456789abcdef
+// base 16 abcdef
+// if (input[i] == 'X')//0123456789ABCDEF
+//	base 16 ABCDEF
+// if (input[i] == '%')
+//	ft_putchar('%');
+// somar o valor dos returns
